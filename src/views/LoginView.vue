3611298aa450b2cdad0d5c0d48d293078da8656f
hoseinx <hoseinx@gmail.com>
2025-07-15 15:23:29 +0330

    fix

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -118,6 +118,8 @@ onMounted(() => {
   }));
 
   function draw() {
+    if (!ctx) return;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#ffffff";
     stars.forEach((star) => {
